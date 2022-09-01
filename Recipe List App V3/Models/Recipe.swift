fbbd364ea204719c2a4a3219ba39b2faf36e8536
Jaz998 <Jaz998@users.noreply.github.com>
2022-09-01 14:57:48 +1000

    added class recipe

--- Recipe List App V3/Models/Recipe.swift
+++ Recipe List App V3/Models/Recipe.swift
@@ -7,6 +7,16 @@
 
 import Foundation
 
-class Recipe {
-    
+class Recipe: Identifiable, Decodable {
+    var id:UUID?
+    var name:String
+    var featured:String
+    var image:String
+    var description:String
+    var prepTime:String
+    var cookTime:String
+    var totalTime:String
+    var servings:Int
+    var ingredients:[String]
+    var directions:[String]
 }
