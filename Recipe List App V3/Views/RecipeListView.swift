b4ad6f4628da6a8e9517956a631720f550bfad18
Jaz998 <Jaz998@users.noreply.github.com>
2022-09-01 18:38:13 +1000

    View can now show a list of recipe image and associated names

--- Recipe List App V3/Views/RecipeListView.swift
+++ Recipe List App V3/Views/RecipeListView.swift
@@ -13,8 +13,17 @@ struct RecipeListView: View {
     @ObservedObject var model = RecipeModel()
     
     var body: some View {
-        Text("Hello, everyone!")
-            .padding()
+        List(model.recipes) {r in
+            HStack(spacing: 20.0){
+                Image(r.image)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 50, height: 50, alignment: .center)
+                    .clipped()
+                    .cornerRadius(5)
+                Text(r.name)
+            }
+        }
     }
 }
 
